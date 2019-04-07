890c5a35006137eee49d104e8c713b9abeeec15e
farahriz <41633573+farahriz@users.noreply.github.com>
2019-04-07 21:34:47 +0800

    Fill in skeleton of event card display

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h4>{{ title }}</h4>
+    <div class="event-card -shadow">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h4 class="title">{{ event.title }}</h4>
+      <span>{{ event.attendees.length }} attending</span>
+    </div>
   </div>
 </template>
 
@@ -8,14 +13,39 @@
 export default {
   data() {
     return {
-      title: 'Park Cleanup'
+      event: {
+        id: 1,
+        title: 'Park Cleanup',
+        date: 'Tue May 15 2018',
+        time: '6:00',
+        attendees: [
+          { id: 'abc124', name: 'John Doe' },
+          { id: 'def876', name: 'Joe Bloggs' }
+        ]
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-h4 {
-  color: green;
+.event-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
 }
 </style>
